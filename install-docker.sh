c0d0b60e0c387ad2651dfac7e2ab16e172cc4bbb
Brandon Tuttle <tuttlebr@ncc-1701.biz>
2023-04-26 16:29:51 +0000

    update docker cert to use rsync
    Signed-off-by: Brandon Tuttle <tuttlebr@ncc-1701.biz>

--- install-docker.sh
+++ install-docker.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-
-sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update
-sudo apt-get install \
+
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
+sudo apt-get autoremove -y
+
 sudo mkdir -m 0755 -p /etc/apt/keyrings
+sudo rm -f /etc/apt/keyrings/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
 sudo usermod -aG docker $USER
+
 newgrp docker
--- mk-docker-cert.sh
+++ mk-docker-cert.sh
@@ -4,6 +4,7 @@ PASSWORD=admin
 NAME=discovery
 CERTS_DEST=/volume1/brandon/docker-registry/certs
 AUTH_PATH=/volume1/brandon/docker-registry/auth
+REMOTE_HOSTS="daedalus-02 daedalus-03"
 clear 
 
 rm -f ${NAME}.* htpasswd
@@ -13,7 +14,17 @@ chmod 400 ${NAME}.key
 sudo openssl req -new -x509 -nodes -sha1 -days 365 -key ${NAME}.key -out ${NAME}.crt -config san.cnf
 docker run --rm --entrypoint htpasswd registry:2.7.0 -Bbn ${USERNAME} ${PASSWORD} > htpasswd
 
-cp -p htpasswd ${AUTH_PATH}
+rsync --rsync-path="sudo rsync" -apv htpasswd ${AUTH_PATH}
 sudo mkdir -p /etc/docker/certs.d/${NAME}.local:31500
-sudo cp -p ${NAME}.crt /etc/docker/certs.d/${NAME}.local:31500/ca.crt
-sudo cp -p ${NAME}.* ${CERTS_DEST}
+rsync --rsync-path="sudo rsync" -apv ${NAME}.crt /etc/docker/certs.d/${NAME}.local:31500/ca.crt
+rsync --rsync-path="sudo rsync" -apv ${NAME}.* ${CERTS_DEST}
+
+for REMOTE_HOST in ${REMOTE_HOSTS};
+do 
+    echo "Setting up ${REMOTE_HOST}"
+    ssh ${REMOTE_HOST} sudo mkdir -p /etc/docker/certs.d/${NAME}.local:31500
+    rsync --rsync-path="sudo rsync" -apv ${NAME}.crt ${REMOTE_HOST}:/etc/docker/certs.d/${NAME}.local:31500/ca.crt
+done
+
+echo "Docker cert creation completed."
+echo "Restart the docker registry server to pick up new credentials."
